a6eb1e9f40027b3792527d0453f574b0c893ae8e
Jacob Steffler <jacobsteffler@gmail.com>
2020-03-04 10:44:41 -0600

    Create basic team/event/match models

--- nilus/season2020/models.py
+++ nilus/season2020/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    number = models.PositiveIntegerField(unique=True)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return str(self.number) + ' - ' + self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=256)
+    teams = models.ManyToManyField(Team)
+
+    def __str__(self):
+        return self.name
+
+class Match(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    # Don't force unique to allow for rematches.
+    # Use a CharField so we can use things like 'Semifinal 1 Match 2'
+    number = models.CharField(max_length=256)
+
+    # TODO figure out limit_choices_to to filter out teams that are not in this event
+    red_one = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='red_one')
+    red_two = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='red_two')
+    red_three = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='red_three')
+
+    blue_one = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='blue_one')
+    blue_two = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='blue_two')
+    blue_three = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='blue_three')
+
+    def __str__(self):
+        return self.number + ' at ' + str(self.event)
+
+    class Meta:
+        verbose_name_plural = "matches"
