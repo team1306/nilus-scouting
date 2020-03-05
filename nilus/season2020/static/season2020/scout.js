74eaeb37bbe6b573643c30420fa268ad739d6c1c
Jacob Steffler <jacobsteffler@gmail.com>
2020-03-05 16:20:09 -0600

    Get increment buttons working

--- nilus/season2020/static/season2020/scout.js
+++ nilus/season2020/static/season2020/scout.js
@@ -11,4 +11,24 @@ $(document).ready(function()
             return false;
         }
    });
+
+   hookUpAdderButton($("#btnAutoLowInc"), $("#inputAutoLow")[0], 1);
+   hookUpAdderButton($("#btnAutoLowDec"), $("#inputAutoLow")[0], -1);
+   hookUpAdderButton($("#btnAutoHighInc"), $("#inputAutoHigh")[0], 1);
+   hookUpAdderButton($("#btnAutoHighDec"), $("#inputAutoHigh")[0], -1);
+   hookUpAdderButton($("#btnTeleLowInc"), $("#inputTeleLow")[0], 1);
+   hookUpAdderButton($("#btnTeleLowDec"), $("#inputTeleLow")[0], -1);
+   hookUpAdderButton($("#btnTeleHighInc"), $("#inputTeleHigh")[0], 1);
+   hookUpAdderButton($("#btnTeleHighDec"), $("#inputTeleHigh")[0], -1);
 });
+
+function hookUpAdderButton(button, fieldEl, increment)
+{
+    button.click(function()
+    {
+        var current = parseInt(fieldEl.value);
+        var newVal = current + increment;
+        if(newVal < 0) { return; }
+        fieldEl.value = newVal;
+    });
+}
