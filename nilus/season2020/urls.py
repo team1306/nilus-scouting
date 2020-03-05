48cc812f6c45a42d3c5da963fdad36c6871a5645
Jacob Steffler <jacobsteffler@gmail.com>
2020-03-05 13:03:39 -0600

    Add url namespace to season2020

--- nilus/season2020/urls.py
+++ nilus/season2020/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'season2020'
 urlpatterns = [
-    path('scout/<int:match_id>/<int:team_num>', views.scout),
+    path('scout/<int:match_id>/<int:team_num>', views.scout, name='scout'),
 ]
