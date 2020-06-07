b7a7568f88d1f1d14c8feec73cceecddcf373f89
dayiethan <30461116+dayiethan@users.noreply.github.com>
2020-06-07 18:45:11 -0500

    Landing page (plus some extra files I'll use later)

--- nilus/nilus/urls.py
+++ nilus/nilus/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('season2020/', include('season2020.urls'))
+    path('season2020/', include('season2020.urls')),
+    path('', views.landing, name="landing")
 ]
