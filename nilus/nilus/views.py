807263372855d160d0ac044cde28519e0f6c855a
dayiethan <30461116+dayiethan@users.noreply.github.com>
2020-06-07 19:51:48 -0500

    Landing page html update

--- nilus/nilus/views.py
+++ nilus/nilus/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def landing(request):
-    return render(request, 'nilus/test.html')
+    return render(request, 'nilus/index.html')
