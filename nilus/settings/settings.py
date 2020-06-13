0a90def14de3d47a0e5f9b84a41e69056b7a9901
eganj <eg.co.johnson@gmail.com>
2020-06-12 19:49:26 -0500

    Project global files

--- nilus/settings/settings.py
+++ nilus/settings/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'globalfiles','templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +71,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'globalfiles', "static"),
 ]
 
 WSGI_APPLICATION = 'settings.wsgi.application'
