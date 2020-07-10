da128913ea4f96248a19cfaa5fc55e5fa7e4053e
eganj <eg.co.johnson@gmail.com>
2020-07-10 10:23:42 -0500

    Search Match Reports

---
+++ nilus/globalfiles/static/global/js/localforagefind.js
@@ -0,0 +1,76 @@
+// Taken from https://github.com/psalaets/localforage-find/blob/master/index.js
+// Documentation https://github.com/psalaets/localforage-find
+;(function(global) {
+    // commonjs environment
+    if (typeof module == 'object' && module.exports) {
+      module.exports = addFind;
+    } else { // plain old <script> tag
+      global.localforageFind = addFind;
+    }
+  
+    var UNLIMITED = -1;
+  
+    function addFind(localforage) {
+      var prototype = Object.getPrototypeOf(localforage);
+      prototype.find = function find(criteria, callbackOrLimit, maybeCallback) {
+        var limit, callback;
+  
+        // limit was specified
+        if (typeof callbackOrLimit == 'number') {
+          limit = callbackOrLimit;
+          callback = maybeCallback;
+        } else { // no limit
+          limit = UNLIMITED;
+          callback = callbackOrLimit;
+        }
+  
+        var lf = this;
+        var promise = lf.keys().then(function(keys) {
+          // no data stored
+          if (!keys.length) return [];
+          // asked for no results
+          if (!limit) return [];
+  
+          var results = [],
+              pairsSeen = 0,
+              pairsExpected = keys.length;
+  
+          return lf.iterate(function(value, key) {
+            if (criteria(key, value)) {
+              results.push(value);
+            }
+  
+            pairsSeen += 1;
+  
+            // Stop iterating and return results if we...
+  
+            // have checked every key/value pair
+            if (pairsSeen == pairsExpected) return results;
+            // or have found enough results
+            if (limit != UNLIMITED && results.length == limit) return results;
+          });
+        });
+  
+        return chainCallback(promise, callback);
+      };
+    }
+  
+    /**
+    * Hook callback into a promise.
+    *
+    * @param promise Promise to chain onto
+    * @param callback Optional error-first callback to invoke with promise
+    * @return a promise
+    */
+    function chainCallback(promise, callback) {
+      if (callback) {
+        return promise.then(function fulfilled(result) {
+          callback(null, result);
+        }, function rejected(reason) {
+          callback(reason);
+        });
+      } else {
+        return promise;
+      }
+    }
+  })(this);
--- nilus/season2020/static/season2020/js/storageWrapper.js
+++ nilus/season2020/static/season2020/js/storageWrapper.js
@@ -4,20 +4,50 @@
  * 
  * The namespace all functionality is included in is called store, and containes the following:
  * 
- *  -reportToKey(scoreReport): takes in a match score report and returns the key. Sets
+ *  -reportToKey({Object} scoreReport): takes in a match score report and returns the key. Sets
  *      storeReport.storageKey to the key for future reference.
  * 
  *  -matchToKey(matchNumber, teamNumber): returns the key for given match and team number, eg "1306"
  *      NOT "red 1". Usefull for situations that do not have the full score report
  *  
- *  -saveReport(report): saves given score report. Returns the localForage promise object.
+ *  -saveReport({Object} report): saves given score report. Returns the localForage promise object.
  * 
- *  -fetchReport(key): returns the localForage promise object to get the score report. The .then
+ *  -fetchReportByKey(key): returns the localForage promise object to get the score report. The .then
  *      function for the promise should take in a function one parameters, for the report value.
+ * 
+ *  -fetchReportsByMatch({int} match): Finds all reports with given match numbers. Returns a 
+ *      promise object who passes an array of all found reports to .then function
+ * 
+ *  -fetchReportsByTeam({int} team): Finds all reports with given team numbers. Returns a promise 
+ *      object who passes an array of all found reports to .then function
+ * 
+ *  -fetchReportsByFunction({Function} evaluator): Finds all (key, value) pairs for whom the eval
+ *      function returns a truthy value. Returns a promise object who passes an array of all found
+ *      reports to .then function
+ * 
+ *  -executeOnEach({Function} callback): Similar to fetchReportsByFunction, calls callback for each
+ *      (key, value) pair in match reports.
+ * 
+ * More extensive documentation can be found at the head of each function.
  */
 
 //namespace container
 var store = {};
+
+//check dependency scripts
+if (localforage) {
+    console.log("Local Forage loaded correctly.")
+    if (localforageFind) {
+        localforageFind(localforage);
+        console.log("Local Forage Find loaded correctly.");
+    } else {
+        throw "Unable to find Local Forage Find!"
+    }
+} else {
+    throw "Unable to find local forage!"
+}
+
+
 
 /**
  * A consistent method for creating keys from match reports.
@@ -27,13 +57,12 @@ var store = {};
  * 
  * Match reports change every year: the only thing that can be relied upon to be included every
  * year that also refers to only one possible report is the combination of match and team. The key
- * is therefore created by combining these two, in the format "matchNumber:teamNumber"
+ * is therefore created by combining these two, in the format "M{matchNumber}:T{teamNumber}"
  * 
  * @param {Object} report- the match report 
  */
-store.reportToKey = function(report){
+store.reportToKey = function (report) {
     let key = store.matchToKey(report.matchNumber, report.teamNumber);
-    console.log(key);
     report.storageKey = key;
     return key;
 }
@@ -43,11 +72,10 @@ store.reportToKey = function(report){
  * 
  * @param matchNumber
  * @param teamNumber - the actual team code, eg "1306" NOT "red 1"
- * @return {String} - the key, which is "matchNumber:teamNumber"
+ * @return {String} - the key, which is "M{matchNumber}:T{teamNumber}", eg "M4:T1306"
  */
-store.matchToKey = function(matchNumber, teamNumber){
-    console.log(matchNumber);
-    return matchNumber+":"+teamNumber;
+store.matchToKey = function (matchNumber, teamNumber) {
+    return "M"+matchNumber + ":T" + teamNumber;
 }
 
 /** 
@@ -55,10 +83,8 @@ store.matchToKey = function(matchNumber, teamNumber){
  * 
  * @param {*} report - the match score report. MUST have fields matchNumber and teamNumber.
  */
-store.saveReport= function(report){
-    console.log(report);
+store.saveReport = function (report) {
     let key = store.reportToKey(report);
-    console.log(key);
     return localforage.setItem(key, report);
 }
 
@@ -66,5 +92,65 @@ store.saveReport= function(report){
  * No extra functionality over the localforage.getItem needed currently. 
  * 
  * Possible additions could include validation of key.
+ * @param {String} key - key for report. Can be generated from store.matchToKey
  */
-store.fetchReport = localforage.getItem;
+store.fetchReportByKey = localforage.getItem;
+
+//Local Forage Find operations
+
+/**
+ * Returns a promise whose .then parameter is all stored match reports with given match number
+ * @param {int} matchNumber
+ * @return a promise. Functions listening to .then should take one paramater, which will be an
+ *          array of any stored match reports with given number.
+ */
+store.fetchReportsByMatch = function (matchNumber) {
+    let evaluateReport = (key, value) => { return value.matchNumber == matchNumber && value.isMatchReport; }
+    return localforage.find(evaluateReport);
+}
+
+/**
+ * Returns a promise whose .then parameter is all stored match reports with given match number
+ * @param {int} teamNumber
+ * @return a promise. Functions listening to .then should take one paramater, which will be an
+ *          array of any stored match reports with given number.
+ */
+store.fetchReportsByTeam = function (teamNumber) {
+    let evaluateReport = (key, value) => { return value.teamNumber == teamNumber && value.isMatchReport; }
+    return localforage.find(evaluateReport);
+}
+
+/**
+ * Returns a promise whose .then parameter containing all match reports accepted by the evaluation
+ * function.
+ * @param {Function} eval - a function with parameters (key, value) through which all reports are
+ *                          passed. If the function returns a truthy value the report will be added
+ *                          to the array given to the .then function.
+ * @returns {Promise} results of the operation
+ */
+store.fetchReportsByFunction = function (evalCallback) {
+    let evaluateReport = (key, value) => {
+        return value.isMatchReport && evalCallback(key, value);
+    }
+    return localforage.find(evaluateReport);
+}
+
+/**
+ * Runs the given callback for every (key, value) pair that is a match report. 
+ * 
+ * NOTE: Changing field in the value parameter does not reflect in the database. Use store.saveReport
+ * @param {Function} callback - takes params (key, value) for each match report
+ */
+store.executeOnEach = function (callback) {
+    // This one is a little bit of a hack. Since finding reports runs an evaluation function on
+    // every item in localForage, it can also be used to execute operations for each element.
+    // All that then remains is to check that an element is a match report before executing on top
+    // of it. 
+    localforage.find((key, value) => {
+        if (value.isMatchReport) {
+            callback(key, value);
+        }
+        // not actually collecting information so don't add anything to the list.
+        return false;
+    });
+}
